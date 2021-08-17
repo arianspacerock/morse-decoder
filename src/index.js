286c227bb2d6b1836e49434e6bb9d84c887497f1
arianspacerock <siropemptyspace@gmail.com>
2021-08-17 11:25:31 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -39,7 +39,38 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    let dash = /(11)/g;
+    let dot = /(10)/g;
+    let zero = /(0)/g;
+    let arrBinary = [];
+    let arrDecode = [];
+
+    for(let i = 0; i < expr.length; i += 10) {
+        arrBinary.push(expr.substr(i, 10));
+    }
+
+    let string = arrBinary.join(' ');
+
+    let strDash = string.replace(dash ,'-');
+    let strDot = strDash.replace(dot, '.');
+    let morse = strDot.replace(zero, '');
+
+    let arrMorse = morse.split(' ');
+
+    for (let j = 0; j < arrMorse.length; j++){
+        if (arrMorse[j] in MORSE_TABLE){
+            arrDecode.push( MORSE_TABLE[arrMorse[j]]);
+        } else {
+            arrDecode.push(' ');
+        }
+    
+    };
+    
+    return arrDecode.join('');
+  
 }
+
+
 
 module.exports = {
     decode
